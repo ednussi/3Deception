f5a91d234a6f8d9bb5ab720b61deb2eedd8bc71a
Gregory Pasternak <griffonn@gmail.com>
2016-11-13 20:13:22 +0200

    add single-option question loading

---
+++ questionnaire/prepare_questions.py
@@ -0,0 +1,31 @@
+from collections import OrderedDict
+import csv
+
+
+def prepare_vocal_single_options(path):
+    """
+    Load questions and answer formats for single-option vocal questions
+    :param path: csv file to load from
+    :return: ordered dictionary where keys are questions and values are answer format that the subject must say out loud
+    """
+    question_templates = OrderedDict()
+
+    with open(path) as out:
+        reader = csv.reader(out)
+
+        headers = next(reader, [])
+
+        if len(headers) != 2 or headers[0] != "question" or headers[1] != "answer_format":
+            raise Exception("prepare_vocal_single_options: Bad csv format")
+
+        for row in reader:
+            if len(row) != 2:
+                raise Exception("prepare_vocal_single_options: Bad csv format")
+
+            question_templates[row[0]] = row[1]
+
+    return question_templates
+
+
+def sv_ordered_dict_to_list(sv_od):
+    return [item for kvp in list(sv_od.items()) for item in kvp]
