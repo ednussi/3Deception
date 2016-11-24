570207fabfd91c3b89da31e96cb01d0c9cb1e17d
Eran Nussinovitch <Eran.Nussinovitc@gmail.com>
2016-11-24 18:35:36 +0200

    Random repeted questions with list of r\g

--- questionnaire/questions.py
+++ questionnaire/questions.py
@@ -64,8 +64,8 @@ def nextQ(rect, colors, sock, root, label, b, q_timeout, a_timeout, q, a):
     # Change rect color accordingly
     rect_color = colors[0]
     colors.pop(0)
-    # print('\n\n'+rect_color)
-    # changeRect(root, rect, rect_color, root.winfo_screenwidth())
+    print('\n\n'+rect_color)
+    #changeRect(root, rect, rect_color, root.winfo_screenwidth())
 
     blank_time = 500
     after_answer_time = 1000
@@ -90,46 +90,6 @@ def nextQ(rect, colors, sock, root, label, b, q_timeout, a_timeout, q, a):
     # curTime = curTime + AintervalTime
 
     root.after(blank_time + a_timeout + blank_time + after_answer_time, place_button, b)
-    # curTime = curTime + blankWindowTime
-
-def nextQMultiple(rect, colors, sock, root, label, b, q_timeout, a_timeout, q, a):
-    b.place_forget()
-    
-    # Change rect color accordingly
-    rect_color = colors[0]
-    colors.pop(0)
-    # print('\n\n'+rect_color)
-    # changeRect(root, rect, rect_color, root.winfo_screenwidth())
-
-    blank_time = 500
-    after_answer_time = 1000
-    # Show blank between questions
-    root.after(0, change_label, label, '')
-    root.after(0, send_record_flag_udp, sock, 0)
-
-    # Show question
-    root.after(blank_time, change_label, label, q)
-    root.after(blank_time, send_record_flag_udp, sock, 1)
-    # curTime = curTime + QintervalTime
-
-    # Show answer format
-    ans_interval = 0
-    ans_time = 3000
-
-    #runs and shows all answers
-    for ans in a:
-        root.after(blank_time + q_timeout + ans_interval, change_label, label, '')
-        root.after(blank_time + q_timeout + blank_time + ans_interval, change_label, label, ans)
-        ans_interval += ans_interval + ans_time
-    # if rect_color == 'RED':
-    #     flag = 3
-    # else:
-    #    flag = 2
-    flag = 2
-    root.after(blank_time + q_timeout + blank_time+ans_interval, send_record_flag_udp, sock, flag)
-    # curTime = curTime + AintervalTime
-
-    root.after(blank_time + a_timeout + blank_time + after_answer_time+ans_interval, place_button, b)
     # curTime = curTime + blankWindowTime
 
 def place_button(b):
@@ -159,8 +119,6 @@ def next_question(receiver, root, rect, sock, questions, colors):
     return (q, a)
 
 def main():
-    #TEST_TYPE = 'SINGLE_OPTION' or TEST_TYPE = 'MULTIPLE_OPTION'
-    TEST_TYPE = 'MULTIPLE_OPTION'
 
     # Creates the full screen and puts empty first label at top
     root, ws, hs = set_window_mid_screen()
@@ -168,13 +126,25 @@ def main():
     # label.grid(row=1,column=2)
     label.place(relx=0.5, rely=0, anchor=tk.N)
     
+    # Get twice as much questions (half for true half for lies)
+    # and sets colors to be tag for lie\truth
     q_list = assoc_array_to_list(prepare_vocal_single_option("data/questions_slt.csv"))
+    q_amount = int(len(q_list)/2)
+    truth_lies_multiplyer = 2
+    q_list = q_list * truth_lies_multiplyer #get questions twice
+    colors = ['RED']*q_amount + ['GREEN']*q_amount #get equal amount of true\lie amounts
 
-    tq = list(zip(q_list[::2], q_list[1::2]))
+    # Times for repetation of questions:
+    rep = 4
+    colors = colors*rep
+    q_list = q_list*rep
+
+    tq = list(zip(q_list[::2], q_list[1::2], colors))
     random.shuffle(tq)
+    colors = [q[-1] for q in tq] #get colors out
+    tq = [q[:2] for q in tq] #return tq to tupples of 2
     tq = [q for t in [p for p in tq] for q in t]
-
-    colors = [random.choice(('RED', 'GREEN')) for x in tq]
+    print(tq)
 
     receiver = subprocess.Popen(['python', 'fs_receive.py'])
     sock = connect_to_fs_receiver_udp()
@@ -196,48 +166,6 @@ def main():
     send_record_flag_udp(sock, 10)
 
     root.mainloop()
-
-# curTime = 1000
-# QintervalTime = 1000
-# AintervalTime = 1000
-# blankWindowTime = 1000
-
-# for q in range(int(len(q_list) / 2)):
-
-#     # Show question
-#     root.after(curTime,change_label, label, q_list[q*2])
-#     curTime = curTime + QintervalTime
-
-#     # Show answer format
-#     root.after(curTime,change_label, label, q_list[q*2+1])
-#     curTime = curTime + AintervalTime
-
-#     # Show blank between questions
-#     root.after(curTime,change_label, label, '')
-#     curTime = curTime + blankWindowTime
-
-# # for q in range(int(len(q_list) / 2)):
-# #     root.after(curTime,change_label, label, q_list[q*6])
-# #     curTime = curTime + QintervalTime
-
-# #     for a in range(4):
-# #         print("size of q_list is: %d"%len(q_list))
-# #         print("q: %d, a: %d, q*6+a: %d"%(q ,a,q*6+a))
-# #         root.after(curTime,change_label, label, q_list[q*6+a])
-# #         curTime = curTime + AintervalTime
-
-# #         # Show blank window
-# #         root.after(curTime,change_label, label, '')
-# #         curTime = curTime + blankWindowTime
-
-# #     #Show Question Again
-# #     questString = q_list[q*6]
-# #     root.after(curTime, change_label, label, questString)
-# #     curTime = curTime + QintervalTime
-
-
-
-# root.mainloop()
 
 if __name__ == "__main__":
     main()
