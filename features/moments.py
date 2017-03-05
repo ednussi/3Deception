0da07f90eef90999bc11f73b7ba1ae33d2884543
Gregory Pasternak <griffonn@gmail.com>
2017-03-05 13:44:28 +0200

    start features

---
+++ features/moments.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from scipy import stats as sp
+
+
+def moments(df: pd.DataFrame) -> pd.DataFrame:
+    df_mean = df.drop(['timestamp'], axis=1).groupby(['record_flag']).mean()
+    df_var = df.drop(['timestamp'], axis=1).groupby(['record_flag']).var()
+    df_skew = df.drop(['timestamp'], axis=1).groupby(['record_flag']).aggregate(sp.skew, bias=False)
+    df_kurt = df.drop(['timestamp'], axis=1).groupby(['record_flag']).aggregate(sp.kurtosis, bias=False)
+
+    return pd.concat([df_mean, df_var, df_skew, df_kurt], axis=1)
