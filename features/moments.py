1f7acad6125011c544c50ad89bc174b0a2cc4f26
Gregory Pasternak <griffonn@gmail.com>
2017-03-08 17:56:31 +0200

    WIP: dynamic features

--- features/moments.py
+++ features/moments.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy import stats as sp
-from sklearn import cluster as skcluster
+from sklearn import cluster as sk_cluster
 
 
 def moments(df: pd.DataFrame) -> pd.DataFrame:
@@ -13,13 +13,36 @@ def moments(df: pd.DataFrame) -> pd.DataFrame:
     return pd.concat([df_mean, df_var, df_skew, df_kurt], axis=1)
 
 
-def dynamic_quantized(df: pd.DataFrame, n_quant) -> pd.DataFrame:
+def quantize(df: pd.DataFrame, n_quant) -> pd.DataFrame:
     question_dfs = []
     for qdf in df.drop(['timestamp'], axis=1).groupby(['question']):
-        question_df_aus = qdf[1].iloc[:, 2:]
-        for au in question_df_aus:
-            question_df_aus[au] = skcluster.KMeans(n_clusters=n_quant, random_state=1).fit_predict(
-                np.reshape(question_df_aus[au].values, (-1, 1)))
-        question_dfs.append(question_df_aus)
+        for au in qdf[1].iloc[:, 2:]:
+            qdf[1].loc[:, au] = sk_cluster.KMeans(n_clusters=n_quant, random_state=1).fit_predict(
+                np.reshape(qdf[1][au].values, (-1, 1)))
+        question_dfs.append(qdf)
 
-    return pd.concat(question_dfs, axis=0)
+    return question_dfs
+
+
+def dynamic_activations(question_dfs: [pd.DataFrame]) -> pd.DataFrame:
+    question_ratios = []
+    question_levels = []
+    question_length = []
+    question_average_volumes = []
+
+    # for each question df
+    for qdf in question_dfs:
+        # for each AU in the question
+
+        # Activation ratio (#frames AU is nonzero / #frames)
+        question_ratios.append((qdf[0], qdf[1].astype(bool).sum() / qdf[1].count()))
+
+        # Activation mean level
+        question_levels.append((qdf[0], qdf[1].mean()))
+
+        # TODO Activation mean length
+
+        # AU average volume
+        question_average_volumes.append(pd.Series(qdf[1].astype(bool).sum().mean(axis=1), name='average_volume'))
+
+    return question_ratios, question_levels, question_length, question_average_volumes
