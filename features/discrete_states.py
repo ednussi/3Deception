e886ff93f1549a628adfaafb06683e0b41a28fad
Gregory Pasternak <griffonn@gmail.com>
2017-03-12 00:28:40 +0200

    Minor

--- features/discrete_states.py
+++ features/discrete_states.py
@@ -1,21 +1,5 @@
 import pandas as pd
-import numpy as np
-from sklearn import cluster as sk_cluster
-import features.utils as utils
-
-
-def quantize(question_dfs: list(pd.DataFrame), n_quant: int) -> list(pd.DataFrame):
-    question_quantized_dfs = []
-
-    for q_df in question_dfs:
-        q = q_df.copy()
-        for au in q.iloc[:, 2:]:
-            q.loc[:, au] = sk_cluster.KMeans(n_clusters=n_quant, random_state=1)\
-                .fit_predict(np.reshape(q[au].values, (-1, 1)))
-
-        question_quantized_dfs.append(q)
-
-    return question_quantized_dfs
+from features import utils as utils
 
 
 def discrete_states(question_quantized_dfs: list(pd.DataFrame)) -> list(pd.DataFrame):
--- features/utils.py
+++ features/utils.py
@@ -1,11 +1,13 @@
+from sklearn import cluster as sk_cluster
 import pandas as pd
+import numpy as np
 
 
 def split_df_to_questions(df: pd.DataFrame) -> list(pd.DataFrame):
     """
     Split raw data frame by questions
     """
-    return [*map(lambda x: x[1], df.drop(['timestamp'], axis=1).groupby(['question']))]
+    return [t[1] for t in df.drop(['timestamp'], axis=1).groupby(['question'])]
 
 
 def scale(val):
@@ -19,3 +21,16 @@ def scale(val):
         return val
     return ((val - min(val)) / (max(val)-min(val))) * (top-bot) + bot
 
+
+def quantize(question_dfs: list(pd.DataFrame), n_quant: int) -> list(pd.DataFrame):
+    question_quantized_dfs = []
+
+    for q_df in question_dfs:
+        q = q_df.copy()
+        for au in q.iloc[:, 2:]:
+            q.loc[:, au] = sk_cluster.KMeans(n_clusters=n_quant, random_state=1)\
+                .fit_predict(np.reshape(q[au].values, (-1, 1)))
+
+        question_quantized_dfs.append(q)
+
+    return question_quantized_dfs
