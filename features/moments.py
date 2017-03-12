650e1a60d79aa2def997837e16df7a9a723bed13
Gregory Pasternak <griffonn@gmail.com>
2017-03-12 11:56:07 +0200

    Fix moments

--- features/moments.py
+++ features/moments.py
@@ -1,16 +1,10 @@
 import pandas as pd
-from scipy import stats as sp
 
 
-def moments(question_dfs: list(pd.DataFrame)) -> list(pd.DataFrame):
+def moments(question_dfs):
     question_moments = []
 
     for q in question_dfs:
-        question_moments.append(pd.concat([
-            q.mean(),
-            q.var(),
-            q.aggregate(sp.skew, bias=False),
-            q.aggregate(sp.kurtosis, bias=False)
-        ], axis=1))
+        question_moments.append(pd.concat([q.mean(), q.var(), q.skew(), q.kurt()], axis=1))
 
     return question_moments
