8f38b23d218fc292fe19327e0591f4eb17a2ff0e
Gregory Pasternak <griffonn@gmail.com>
2017-03-12 12:58:26 +0200

    Finish moments

--- features/moments.py
+++ features/moments.py
@@ -5,6 +5,18 @@ def moments(question_dfs):
     question_moments = []
 
     for q in question_dfs:
-        question_moments.append(pd.concat([q.mean(), q.var(), q.skew(), q.kurt()], axis=1))
+        q_mean = q.mean()
+        q_mean.index = [col + '_mean' for col in q_mean.index]
 
-    return question_moments
+        q_var = q.var()
+        q_var.index = [col + '_var' for col in q_var.index]
+
+        q_skew = q.skew()
+        q_skew.index = [col + '_skew' for col in q_skew.index]
+
+        q_kurt = q.kurt()
+        q_kurt.index = [col + '_kurt' for col in q_kurt.index]
+
+        question_moments.append(pd.concat([q_mean, q_var, q_skew, q_kurt], axis=0))
+
+    return pd.concat(question_moments, axis=1).T
