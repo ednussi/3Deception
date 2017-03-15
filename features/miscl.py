a929f25218772391fd342d172cd81a8ecd4f8388
Eran Nussinovitch <eran.nussinovitc@mail.huji.ac.il>
2017-03-15 11:41:30 +0200

    Added misclenious and utils funcs

---
+++ features/miscl.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from features.utils import count_peaks
+
+
+
+def miscl(question_dfs):
+    '''
+    # of Smiles
+    Calculated by taking the maximum of the amount of peaks in the
+    signals of both lip corners, where peak is defined as a local minimum which is higher by at
+    least 0.75 as compared to its surrounding points.
+
+    # of Blinks
+    The amount of blinks was calculated in a
+    similar manner, with a threshold of 0.2.
+
+    '''
+
+    num_smiles =[]
+    num_blinks = []
+    ans_miscl = []
+    for ans in question_dfs:
+        th = 0.75 #smile maximum threshold
+        # Smiles
+        mouth_smiles = ans[:,'MouthSmile_L':'MouthSmile_R']
+        # TODO Maybe seperate to left-right-center smile
+        num_smiles.append(max(count_peaks(ans.loc[:, 'MouthSmile_L'].tolist(), delta=th),
+            count_peaks(ans.loc[:, 'MouthSmile_R'].tolist(), delta=th)))
+
+        # Blinks
+        blinks = ans[:, 'EyeBlink_L':'EyeBlink_R']
+        # TODO Maybe seperate to left-right blinks
+        num_blinks.append(max(count_peaks(ans.loc[:, 'EyeBlink_L'].tolist()),
+            count_peaks(ans.loc[:, 'EyeBlink_R'].tolist())))
+
+    return ans_miscl.append(pd.concat(pd.DataFrame([num_smiles,num_blinks],columns=['smiles','blinks'])))
+
+
+
--- features/utils.py
+++ features/utils.py
@@ -58,4 +58,80 @@ def runs_of_ones_list(bits):
     Returns:
         array of lengths
     """
+
+
     return [sum(g) for b, g in itertools.groupby(bits) if b]
+
+
+def find_peaks(v, delta=0.1, x = None):
+    """
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    Returns two arrays
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    %PEAKDET Detect peaks in a vector
+    %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    %        maxima and minima ("peaks") in the vector V.
+    %        MAXTAB and MINTAB consists of two columns. Column 1
+    %        contains indices in V, and column 2 the found values.
+    %
+    %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    %        in MAXTAB and MINTAB are replaced with the corresponding
+    %        X-values.
+    %
+    %        A point is considered a maximum peak if it has the maximal
+    %        value, and was preceded (to the left) by a value lower by
+    %        DELTA.
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    """
+    maxtab = []
+    mintab = []
+
+    if x is None:
+        x = np.arange(len(v))
+
+    v = np.asarray(v)
+
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+
+    if not np.isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+
+    if delta < 0:
+        sys.exit('Input argument delta must be positive')
+
+    mn, mx = np.Inf, -np.Inf
+    mnpos, mxpos = np.NaN, np.NaN
+
+    lookformax = True
+
+    for i in np.arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+
+        if lookformax:
+            if this < mx-delta:
+            # if this < delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn+delta:
+            # if this > delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+
+    # return np.array(maxtab), np.array(mintab)
+    return np.array(maxtab)
+
+def count_peaks(v, delta=0.1, x = None):
+    return len(find_peaks(v, delta, x))
