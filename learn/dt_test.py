5a39de6eed7dde4e90f5a3447ebe6c06f2a8fe6b
Gregory Pasternak <griffonn@gmail.com>
2017-04-05 00:15:39 +0300

    update dt_test

--- learn/dt_test.py
+++ learn/dt_test.py
@@ -1,7 +1,10 @@
 os.chdir(r'\users\gregory\dropbox\code\3deception')
 import subprocess
+import random
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
-dt = DecisionTreeClassifier(min_samples_split=1, random_state=347)
+
+dt = DecisionTreeClassifier(min_samples_split=1, max_depth=4, random_state=347)
+
 raw = pd.read_csv('grisha_features.csv')
 raw_true = raw[raw.record_flag == 3]
 raw_false = raw[raw.record_flag == 4]
@@ -9,20 +12,30 @@ train_true = raw_true[:20]
 test_true = raw_true[20:]
 train_false = raw_false[:100]
 test_false = raw_false[100:]
-train = pd.concat([train_false, train_true])
-test = pd.concat([test_false, test_true])
 features = raw.columns[4:].tolist()
-labels_train = [False]*train_false.shape[0] + [True]*train_true.shape[0]
-labels_test = [False]*test_false.shape[0] + [True]*test_true.shape[0]
-dt.fit(train[features].values.tolist(), labels_train)
+train = pd.concat([train_false, train_true])
+train_data = train[features].values.tolist()
+test = pd.concat([test_false, test_true])
+test_data = test[features].values.tolist()
+labels_train = [0]*train_false.shape[0] + [1]*train_true.shape[0]
+labels_test = [0]*test_false.shape[0] + [1]*test_true.shape[0]
+train_with_labels = list(zip(train_data, labels_train))
+test_with_labels = list(zip(test_data, labels_test))
+random.shuffle(train_with_labels)
+random.shuffle(test_with_labels)
+shuffled_train_data, shuffled_train_labels = zip(*train_with_labels)
+shuffled_test_data, shuffled_test_labels = zip(*test_with_labels)
+
+dt.fit(shuffled_train_data, shuffled_train_labels)
 with open("dt.dot", "w") as f:
-    export_graphviz(dt, out_file=f, feature_names=features)
+    export_graphviz(dt, out_file=f, feature_names=features, class_names=['LIE', 'TRUTH'], filled=True, rounded=True, special_characters=True)
 
 command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
 try:
     subprocess.check_call(command)
 except:
     print("Could not run graphviz")
-test_prediction = dt.predict(test[features].values.tolist())
-num_right = sum(np.array(labels_test) == test_prediction)
-accuracy = 1. * num_right / len(test_prediction)
+
+test_prediction = dt.predict(shuffled_test_data)
+acc = sum(np.array(shuffled_test_labels) == test_prediction) / len(test_prediction.tolist())
+print(acc)
