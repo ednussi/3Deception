a8b3a6c4fa0a1d48d9b7938d9abc9328cd8112ca
Gregory Pasternak <griffonn@gmail.com>
2017-04-26 13:58:24 +0300

    WIP: search for svm parameters

--- learn/__init__.py
+++ learn/__init__.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import train_test_split, cross_val_score
 import pandas as pd
+import numpy as np
 from features.utils import SKIP_COLUMNS
 from questionnaire.fs_receive import RecordFlags
 
@@ -19,3 +20,31 @@ def cross_validate_roc_auc(classifier, data, target, folds):
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
     return scores
+
+
+def train_svm(classifier, data_path):
+    train, test, train_labels, test_labels = prepare_data(data_path, 0.25)
+
+    classifier.fit(train, train_labels)
+    predicted = classifier.predict(test)
+
+    accuracy = 1. * sum(abs(predicted.astype(int) - test_labels.astype(int))) / len(test_labels)
+    print('Accuracy: {}'.format(accuracy))
+
+
+def try_svms():
+    train, test, train_labels, test_labels = prepare_data(data_path, 0.25)
+    n_features = train.shape[1]
+
+
+
+    for kernel in ['linear', 'poly', 'rdb', 'sigmoid', 'precomputed']:
+        if kernel == 'poly':
+            for degree in range(1,10):
+
+        if kernel == 'poly' or kernel == 'rbf' or kernel == 'sigmoid':
+            for gamma in np.linspace(n_features,1,10):
+
+        if 
+
+
