99e89737b66466ddf66bd1f33dcfaf78a8168263
Gregory Pasternak <griffonn@gmail.com>
2017-05-12 23:39:24 +0300

    WIP graphs

--- runner.py
+++ runner.py
@@ -1,14 +1,103 @@
 import argparse
+import numpy as np
 import pandas as pd
 import os.path as path
 from features import utils
 from learn.utils import cv_all_methods
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import operator as o
 
 
 # TODO
 #     AU SELECTION
 #     FEATURE SELECTION (Groups vs global, dimension (size))
 #     PCA (Groups vs global, dimension (size))
+
+
+def cv_all_learners(raw_path, features_path):
+    print("Cross validating all learners...")
+    results = cv_all_methods(features_path)
+
+    results_df = pd.concat([pd.DataFrame(x['results'], index=x['method']) for x in results])
+
+    results_path = path.join(path.dirname(raw_path), "learning-results_" + path.basename(raw_path))
+    print("Saving learning results to {}...".format(results_path))
+    results_df.to_csv(results_path)
+
+    plot_path = results_path.replace('.csv', '.png')
+    print("Plotting learning results to {}...".format(plot_path))
+    save_results_plot(plot_path, results)
+
+
+def save_results_plot(plot_path, results):
+    def barplot(ax, dpoints):
+        """
+        Create a barchart for data across different categories with
+        multiple conditions for each category.
+        
+        @param ax: The plotting axes from matplotlib.
+        @param dpoints: The data set as an (n, 3) numpy array
+        """
+
+        # Aggregate the conditions and the categories according to their
+        # mean values
+        conditions = [(c, np.mean(dpoints[dpoints[:, 0] == c][:, 2].astype(float)))
+                      for c in np.unique(dpoints[:, 0])]
+        categories = [(c, np.mean(dpoints[dpoints[:, 1] == c][:, 2].astype(float)))
+                      for c in np.unique(dpoints[:, 1])]
+
+        # sort the conditions, categories and data so that the bars in
+        # the plot will be ordered by category and condition
+        conditions = [c[0] for c in sorted(conditions, key=o.itemgetter(1))]
+        categories = [c[0] for c in sorted(categories, key=o.itemgetter(1))]
+
+        dpoints = np.array(sorted(dpoints, key=lambda x: categories.index(x[1])))
+
+        # the space between each set of bars
+        space = 0.3
+        n = len(conditions)
+        width = (1 - space) / (len(conditions))
+
+        # Create a set of bars at each position
+        for i, cond in enumerate(conditions):
+            indeces = range(1, len(categories) + 1)
+            vals = dpoints[dpoints[:, 0] == cond][:, 2].astype(np.float)
+            pos = [j - (1 - space) / 2. + i * width for j in indeces]
+            ax.bar(pos, vals, width=width, label=cond,
+                   color=cm.Accent(float(i) / n))
+
+        # Set the x-axis tick labels to be equal to the categories
+        ax.set_xticks(indeces)
+        ax.set_xticklabels(categories)
+        plt.setp(plt.xticks()[1], rotation=90)
+
+        # Add the axis labels
+        ax.set_ylabel("RMSD")
+        ax.set_xlabel("Structure")
+
+        # Add a legend
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(handles[::-1], labels[::-1], loc='upper left')
+
+        plt.savefig(plot_path)
+
+    data = []
+    for mres in results:
+        method = mres['method']
+        for res in mres['results']:
+            estimator = res['estimator']
+            train_score = res['cv_results']['mean_train_score'].values.tolist()
+            test_score = res['cv_results']['mean_test_score'].values.tolist()
+
+            data.append([method, estimator, train_score[0], test_score[0]])
+
+    data = np.array(data)
+    print(data)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    barplot(ax, data[:, 1:])
 
 
 def extract_features(raw_path, with_pca, au, au_num, feat, feat_num, method):
@@ -37,14 +126,8 @@ def extract_features(raw_path, with_pca, au, au_num, feat, feat_num, method):
 
         fpath = pca_path
 
-    print("Learning...")
-    results_df = cv_all_methods(fpath)
-
-    results_path = path.join(path.dirname(raw_path), "learning-results_" + path.basename(raw_path))
-    print("Saving learning results to {}...".format(results_path))
-    results_df.to_csv(results_path)
-
     print("Done.")
+    return fpath
 
 
 if __name__ == "__main__":
