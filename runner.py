aa158ff0cd8425dbf4ddd458a3d247b56dd6a3e3
Gregory Pasternak <griffonn@gmail.com>
2017-05-13 11:39:34 +0300

    cv all learners by method

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import os.path as path
 from features import utils
-from learn.utils import cv_all_methods
+from learn.utils import cv_method_all_classifiers
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import operator as o
@@ -15,9 +15,9 @@ import operator as o
 #     PCA (Groups vs global, dimension (size))
 
 
-def cv_all_learners(raw_path, features_path):
+def cv_method_all_learners(raw_path, features_path, method, metric=None):
     print("Cross validating all learners...")
-    results = cv_all_methods(features_path)
+    results = cv_method_all_classifiers(features_path, method, metric)
 
     results_df = pd.concat([pd.DataFrame(x['results'], index=x['method']) for x in results])
 
@@ -25,7 +25,7 @@ def cv_all_learners(raw_path, features_path):
     print("Saving learning results to {}...".format(results_path))
     results_df.to_csv(results_path)
 
-    plot_path = results_path.replace('.csv', '.png')
+    plot_path = results_path.replace('.csv', '.{}.png'.format(method))
     print("Plotting learning results to {}...".format(plot_path))
     save_results_plot(plot_path, results)
 
@@ -83,15 +83,13 @@ def save_results_plot(plot_path, results):
         plt.savefig(plot_path)
 
     data = []
-    for mres in results:
-        method = mres['method']
-        for res in mres['results']:
-            estimator = res['estimator']
-            train_score = res['cv_results']['mean_train_score'].values.tolist()
-            test_score = res['cv_results']['mean_test_score'].values.tolist()
+    for res in mres['results']:
+        estimator = res['estimator']
+        train_score = res['cv_results']['mean_train_score'].values.tolist()
+        test_score = res['cv_results']['mean_test_score'].values.tolist()
 
-            data.append([method, estimator + ' mean_train_score', train_score[0]])
-            data.append([method, estimator + ' mean_test_score', test_score[0]])
+        data.append([estimator, 'mean_train_score', train_score[0]])
+        data.append([estimator, 'mean_test_score', test_score[0]])
 
     data = np.array(data)
 
@@ -126,7 +124,6 @@ def extract_features(raw_path, with_pca, au, au_num, feat, feat_num, method):
 
         fpath = pca_path
 
-    print("Done.")
     return fpath
 
 
@@ -134,19 +131,24 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-i', '--input', dest='raw_path')
+    
     parser.add_argument('-p', '--pca', dest='pca', type=int, default=None)
 
     # Possible Choices: 'daniel' 'mouth' 'eyes' 'brows' 'eyes_area' 'smile' 'blinks' 'top'
     parser.add_argument('-a', '--au_choice', dest='au', type=str, default=None)
+    
     # If au was chosen 'top' need a number for number of top AU
     parser.add_argument('-an', '--au_top_num', dest='au_num', type=int, default=24)
 
     # Possible Choices: 'group' 'all'
     parser.add_argument('-f', '--features', dest='feat', type=str, default=None)
+    
     # top num of AU
     parser.add_argument('-fn', '--features_num', dest='feat_num', type=int, default=24)
-    # Choices:
+    
     parser.add_argument('-m', '--method', dest='method', type=str, default='4v1_A', chocies=['4v1_A','4v1_T','4v1_F','SP'])
+
+    parser.add_argument('-mr', '--metric', dest='metric', type=str, default=None)
 
 
     args = parser.parse_args()
@@ -155,7 +157,24 @@ if __name__ == "__main__":
         print(r"Input csv doesn't exist: {}".format(args.raw_path))
         exit()
 
-    extract_features(args.raw_path, args.pca,args.au,args.au_num,args.feat,args.feat_num,args.method)
+    features_path = extract_features(
+        args.raw_path, 
+        args.pca,
+        args.au,
+        args.au_num,
+        args.feat,
+        args.feat_num,
+        args.method
+        )
+
+    cv_method_all_learners(
+        args.raw_path, 
+        features_path, 
+        args.method,
+        metric=None
+        )
+
+
 
 """
 import os
