c2466da4af772ec636e1e0269fa20da82855d9ab
Gregory Pasternak <griffonn@gmail.com>
2017-07-15 15:01:21 +0300

    minor

--- megarunner.sh
+++ megarunner.sh
@@ -5,9 +5,9 @@ do
 		for pc in 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 	  	do
 	  		echo "MEGARUNNER iteration: AU=$au FE=$fe PCA=$pc"
-	  		srun -c4 --mem=2048 --time=3-0 /cs/engproj/3deception/grisha/venv/bin/python3 runner2.py -i questionnaire/data/alon/fs_shapes.not_quantized.csv -atop -A$au -fall -F$fe -pglobal -P$pc -lQSP -nmax &
+	  		/cs/engproj/3deception/grisha/venv/bin/python3 runner2.py -i questionnaire/data/alon/fs_shapes.not_quantized.csv -atop -A$au -fall -F$fe -pglobal -P$pc -lQSP -nmax &
 
-	  		srun -c4 --mem=2048 --time=3-0 /cs/engproj/3deception/grisha/venv/bin/python3 runner2.py -i questionnaire/data/alon/fs_shapes.not_quantized.csv -atop -A$au -fall -F$fe -pglobal -P$pc -lSP -nmax &
+	  		/cs/engproj/3deception/grisha/venv/bin/python3 runner2.py -i questionnaire/data/alon/fs_shapes.not_quantized.csv -atop -A$au -fall -F$fe -pglobal -P$pc -lSP -nmax &
 	  	done
 	done
 done
